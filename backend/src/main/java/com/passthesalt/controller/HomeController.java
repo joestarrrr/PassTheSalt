ea7815467733c4bdb8f39c96e48e1377ff815cb5
venujan nagendirakumar <venujan25@outlook.com>
2026-04-23 09:58:22 +0200

    Controller test

--- backend/src/main/java/com/passthesalt/controller/HomeController.java
+++ backend/src/main/java/com/passthesalt/controller/HomeController.java
@@ -4,12 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController {
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Backend is running!";
     }
-
 }
 
