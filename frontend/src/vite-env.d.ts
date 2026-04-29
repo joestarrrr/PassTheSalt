df00a8d160fc7ebcd58f19d18d50a6070f12e0a6
venujan nagendirakumar <venujan25@outlook.com>
2026-04-29 15:49:32 +0200

    Add types

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_BACKEND_URL?: string
+  readonly VITE_BASE_PATH?: string
   readonly VITE_CLERK_PUBLISHABLE_KEY: string
   readonly VITE_MAPBOX_TOKEN: string
   readonly BASE_URL: string
@@ -9,3 +11,9 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
+
+declare global {
+  const __BACKEND_URL__: string | undefined
+}
+
+export {}
